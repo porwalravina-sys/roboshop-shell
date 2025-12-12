fb9f70b7c5ada5e50e83132e98137eca4ae5b6ea
Ravina <porwal.ravina@gmail.com>
2025-12-12 14:03:36 +0400

    first shell script

--- catalogue.sh
+++ catalogue.sh
@@ -30,6 +30,6 @@ systemctl start catalogue
 
 
 dnf install mongodb-mongosh -y
-mongosh --host mongodb-dev.azdevopsb82.online </app/db/master-data.js
+mongosh --host 10.0.0.8 </app/db/master-data.js
 
 
