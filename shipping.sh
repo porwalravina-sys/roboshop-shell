da4c1588ddef7d081defd1ea37b4189ca6821c75
Ravina <porwal.ravina@gmail.com>
2025-12-15 12:13:46 +0400

    first shell script

--- shipping.sh
+++ shipping.sh
@@ -24,6 +24,6 @@ mysql -h mysql-dev.azdevopsb82.online -uroot -pRoboShop@1 < /app/db/schema.sql
 
 mysql -h mysql-dev.azdevopsb82.online -uroot -pRoboShop@1 < /app/db/app-user.sql
 
-mysql -h mysql.azdevopsb82.online -uroot -pRoboShop@1 < /app/db/master-data.sql
+mysql -h mysql-dev.azdevopsb82.online -uroot -pRoboShop@1 < /app/db/master-data.sql
 
 systemctl restart shipping
