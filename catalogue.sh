6486687f82f4bdfe9e0a8a2f029cef66ad9ca3bd
Ravina <porwal.ravina@gmail.com>
2025-12-15 13:39:08 +0400

    first shell script

--- catalogue.sh
+++ catalogue.sh
@@ -1,34 +1,20 @@
-
-cp catalogue.service /etc/systemd/system/catalogue.service
+app_name = catalogue
+cp $app_name.service /etc/systemd/system/$app_name.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
-
 dnf install nodejs -y
-
 useradd roboshop
-
 rm -rf /app
-
 mkdir /app
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
-unzip /tmp/catalogue.zip
-
+unzip /tmp/$app_name.zip
 cd /app
 npm install
-
-
-
 systemctl daemon-reload
-
-systemctl enable catalogue
-systemctl start catalogue
-
-
-
+systemctl enable $app_name
+systemctl start $app_name
 dnf install mongodb-mongosh -y
 mongosh --host mongodb-dev.azdevopsb82.online </app/db/master-data.js
 
