1b54ad0d17cc9c2994a75e984de4d0f56a968240
Ravina <porwal.ravina@gmail.com>
2025-12-20 15:40:45 +0400

    first shell script

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+source ./common.sh
+
+echo Install MySQL Service
 dnf install mysql-server -y
 Status_Print $?
 
-
+echo Start MySQL Service
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
 Status_Print $?
 
-
+echo Set MySQL Password
 mysql_secure_installation --set-root-pass RoboShop@1
 Status_Print $?
--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,14 +1,20 @@
+#!/bin/bash
+source ./common.sh
+
+echo copy RabbitMQ repo file
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>log_file
 Status_Print $?
 
+echo install RabbitMQ
 dnf install rabbitmq-server -y &>>log_file
 Status_Print $?
 
+echo start RabbitMQ Service
 systemctl enable rabbitmq-server &>>log_file
 systemctl start rabbitmq-server &>>log_file
 Status_Print $?
 
-
+echo Application User
 rabbitmqctl add_user roboshop roboshop123 &>>log_file
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>log_file
 Status_Print $?
